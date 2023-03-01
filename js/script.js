32f8117fea34d3effcf7304f924dd772585c847e
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-03-01 13:19:08 +0100

    feat: exercise completed with for cycle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("ciao")
+const listaSpesa = ["latte", "cereali", "carne", "birra", "sigarette"];
+
+console.log(listaSpesa);
+
+const containerElement = document.getElementById("container");
+
+
+// provo a farlo con un ciclo for
+for(let i = 0; i < listaSpesa.length; i++) {
+
+  // genero un elemento html di tag <li>
+  let newListElement = document.createElement('li');
+  newListElement.innerText = listaSpesa[i];
+
+  // lo imposto come figlio di containerElement
+  containerElement.append(newListElement);
+}
